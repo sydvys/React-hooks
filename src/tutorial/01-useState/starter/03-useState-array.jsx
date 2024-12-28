d159d31bb270ec7984e366ee72fa7f7a8848a3b4
sydv-ys <aysu.seyidova@kibrit.tech>
2024-12-28 14:55:24 +0400

    useState

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -4,6 +4,11 @@ import { data } from "../../../data";
 const UseStateArray = () => {
   const [people, setPeople] = useState(data);
 
+  const removeItem = (id) => {
+    console.log("id", id);
+    const list = people.filter((people) => people.id !== id);
+    setPeople(list)
+  };
   const clearAll = () => {
     setPeople([]);
   };
@@ -14,11 +19,15 @@ const UseStateArray = () => {
         return (
           <div key={id}>
             <p>{name}</p>
-            <button type="button">delete</button>
+            <button type="button" onClick={() => removeItem(id)}>
+              delete
+            </button>
           </div>
         );
       })}
-      <button type="button" onClick={clearAll}>delete all</button>
+      <button type="button" onClick={clearAll}>
+        delete all
+      </button>
     </>
   );
 };
