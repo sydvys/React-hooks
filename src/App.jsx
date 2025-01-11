a607b8004f6d51704bd5b7184ecac8a455ee9955
sydvys <aysuseyidova36@gmail.com>
2025-01-11 19:25:13 +0400

    clean-up

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,9 @@ import LatestValue from "./tutorial/01-useState/starter/05-useState-gotcha";
 import UseEffect from "./tutorial/02-useEffect/starter/04-fetch-data";
 import ConditionalRendering from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
 import MultipleReturnsFetchData from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
-import ShortCircutOverview from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
-import ToggleChallange from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge";
-import UserChallange from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
-// import UserChallangef from "./tutorial/03-conditional-rendering/final/07-user-challenge";
+import ShortCircuitOverview from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
+import ToggleChallenge from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge";
+import UserChallenge from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
 
 
 function App() {
@@ -21,8 +20,8 @@ function App() {
       {/* <UseEffect /> */}
       {/* <ConditionalRendering /> */}
       {/* <MultipleReturnsFetchData /> */}
-      {/* <ToggleChallange /> */}
-      <UserChallange />
+      {/* <ToggleChallenge /> */}
+      <UserChallenge />
     </div>
   );
 }
